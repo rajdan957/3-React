1c03cbb3cf19d838b376dfa3c2b2f8db77bd7427
Raj Dangol <rajdangol@hotmail.com>
2023-05-27 13:14:16 -0400

    week 1

--- src/App.js
+++ src/App.js
@@ -1,56 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import Footer from './components/Footer';
+import Header from './components/Header';
 import './App.css';
+// import CampsiteCard from './features/campsites/CampsiteCard.js';
+import CampsitesList from './features/campsites/CampsitesList';
+// import { CAMPSITES } from './app/shared/CAMPSITES';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <Header />
+      <CampsitesList />
+      <Footer />
     </div>
   );
 }
