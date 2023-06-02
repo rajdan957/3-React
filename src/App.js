7abd5f51ab4c76ec3781276c92b556104642b282
Raj Dangol <rajdangol@hotmail.com>
2023-06-01 21:31:05 -0400

    Router Setup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
+import {Routes, Route} from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import ContactPage from './pages/ContactPage';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import './App.css';
 // import CampsiteCard from './features/campsites/CampsiteCard.js';
-import CampsitesList from './features/campsites/CampsitesList';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 // import { CAMPSITES } from './app/shared/CAMPSITES';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <CampsitesList />
+      <Routes>
+        <Route path='/' element={<HomePage />}/>
+        <Route path='contact' element={<ContactPage />}/>
+        <Route path='directory' element={<CampsitesDirectoryPage />}/>
+      </Routes>
       <Footer />
     </div>
   );
