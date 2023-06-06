472c320fe023d936f2b5c7b7915e7ed71d46cf27
Raj Dangol <rajdangol@hotmail.com>
2023-06-05 22:43:33 -0400

    AnimatedDisplayCard

--- src/features/display/DisplayList.js
+++ src/features/display/DisplayList.js
@@ -1,5 +1,6 @@
 import { Col, Row } from "reactstrap";
-import DisplayCard from './DisplayCard';
+import AnimatedDisplayCard from "./AnimatedDisplayCard";
+// import DisplayCard from './DisplayCard';
 import { selectFeaturedCampsite } from "../campsites/campsitesSlice";
 import { selectFeaturedPromotion } from "../promotions/promotionsSlice";
 import { selectFeaturedPartner } from "../partners/partnersSlice";
@@ -10,9 +11,11 @@ const DisplayList = () => {
         <Row>
             {items.map((item, idx) => {
                 return (
+                    item &&(
                     <Col md className="m-1" key={idx}>
-                        <DisplayCard item={item} />
+                        <AnimatedDisplayCard item={item} />
                     </Col>
+                    )
                 )
             })}
         </Row>
